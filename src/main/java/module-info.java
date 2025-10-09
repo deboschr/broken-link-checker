693977490d286ef9b281e0b295e9fde55e70cd94
deboschr <debospencher@gmail.com>
2025-10-10 02:28:22 +0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module com.unpar.brokenlinkchecker {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
+
     requires org.jsoup;
+    
     requires java.net.http;
     requires java.desktop;
 
